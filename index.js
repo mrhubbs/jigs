a1b074eb5e0ded635554baea3a27914695995ad3
mrhubbs <semberwj@gmail.com>
2018-11-27 15:23:30 -0500

    Add init functionality and split webpack, browserSync, etc. into separate files.

--- index.js
+++ index.js
@@ -1,182 +1,43 @@
-// Run this from the project directory. E.g. the repo of the website being built.
-
-const path = require('path');
-const metalsmith = require('metalsmith');
-const assets = require('metalsmith-assets');
-const inPlace = require('metalsmith-in-place');
-const layouts = require('metalsmith-layouts');
-const ignore = require('metalsmith-ignore');
-const postcss = require('metalsmith-postcss');
-const watch = require('metalsmith-watch');
-const debug = require('metalsmith-debug');
-const webpack = require('metalsmith-webpack');
-
-const browserSync = require('browser-sync');
-
-const { logSuccess, logInfo, logFailure } = require('./lib/logging');
-
-const postcss_config = require(path.join(__dirname, 'postcss.config.js'));
 
 // Determine run mode from CLI
 let mode = process.argv[2];
+
+// initialize a new project
+// (do this before anything else because we try to load a config below, and a
+// new, empty project won't have a config)
+if (mode === 'init') {
+  require('./lib/initer')();
+  process.exit(0);
+}
+
+// default to prototyping, if no mode is specified
 if (mode === undefined) {
   mode = 'prototype';
-} else if (mode !== 'build' && mode !== 'prototype') {
-  logFailure(`Unknown mode "${mode}"`);
+}
+
+// TODO? is this bad
+if (mode === 'prototype') process.env.NODE_ENV = 'development'
+if (mode === 'build') process.env.NODE_ENV = 'production'
+
+// load the configuration
+const log = require('./lib/logging');
+const config = require('./lib/config').load();
+// load the build / prototype functionality
+const builder = require('./lib/metalsmith-builder')
+const webpacker = require('./lib/webpacker')
+const browserSyncer = require('./lib/browserSyncer')
+
+if (mode === 'build') {
+  builder.build(config)
+
+  if (webpacker.shouldUse()) {
+    webpacker.build()
+  }
+} else if (mode === 'prototype') {
+  builder.prototype(config, () => {
+    browserSyncer.start(config, webpacker.middleware)
+  })
+} else {
+  log.logFailure(`Unknown mode "${mode}"`);
   process.exit(1);
-}
-
-const config = require(path.join(__dirname, './lib/config')).load();
-
-const forgeBaseForge = () => {
-  return metalsmith(process.cwd())
-    .metadata(config.metadata)
-    .source(config.dirs.source)
-    .destination(config.dirs.build)
-    .use(debug());
-}
-
-// makes a forge for everything but CSS: HTML, markdown, templates, assets, JS, etc.
-const forgeNonCssForge = () => {
-  let b = forgeBaseForge()
-    .clean(true)
-    // plugins
-    .use(assets({
-      source: config.dirs.assets,
-      destination: config.dirs.assets
-    }))
-    .use(inPlace({
-      suppressNoFilesError: mode === 'prototype',
-      engineOptions: {
-        pattern: ['**/*.ejs', '**/*.md'],
-        // So we don't have to write relative paths to the includes
-        views: [path.resolve(config.dirs.includes)]
-      }
-    }))
-    .use(layouts({
-      directory: config.dirs.layouts,
-      default: config.layouts.default || 'basepage.ejs',
-      pattern: ['**/*.ejs', '**/*.md', '**/*.html', '*.ejs', '*.md', '*.html'],
-      suppressNoFilesError: mode === 'prototype',
-      engineOptions: {
-        // So we don't have to write relative paths to the includes
-        views: [path.resolve(config.dirs.includes)]
-      }
-    }));
-
-    // if the the code section of the config is populated, we'll use webpack
-    // to handle the code-bundling
-    if (config.code !== undefined) {
-      b.use(webpack(
-        require(path.join(__dirname, 'webpack.config.js'))(config)
-      ));
-    }
-
-    return b;
-}
-
-const forgeCssOnlyForge = () => {
-  // create a forge just for building CSS
-  return forgeBaseForge()
-    // run after main forge, don't want to overwrite
-    .clean(false)
-    .use(
-      postcss(
-        postcss_config(
-          config.dirs, { }
-        )
-      )
-    )
-    // Don't want to touch non-CSS
-    // NOTE: we are technically telling this to ignore the CSS folder, but
-    // apparently the postcss plugin ignore this plugin
-    .use(
-      ignore([
-        '*',
-      ])
-    )
-}
-
-let theForge = forgeNonCssForge()
-
-if (mode == 'build') {
-  // building
-
-  // build HTML + JS + assets + whatever else, but not CSS
-  theForge
-    .use(ignore([
-      // Ignore all CSS. We'll leave that for the CSS forge.
-      '**/*.css',
-    ]))
-    .build(function(err) {
-      if (err) throw err;
-
-      logSuccess('Built HTML + JS + assets');
-
-      // build CSS
-      forgeCssOnlyForge()
-        .build(function(err) {
-          if (err) throw err;
-
-          logSuccess('Built CSS');
-        })
-    })
-} else if (mode == 'prototype') {
-  // prototyping
-
-  // add postcss, disabling the plugins that don't play well with prototyping
-  theForge
-    .use(
-      postcss({
-        ...postcss_config(
-          config.dirs, {
-            'postcss-purgecss': false,
-            'postcss-clean': false
-          }
-        ),
-        // load only the main css file; it will import the others
-        pattern: 'main.css'
-      })
-    );
-
-  // set up the forge to watch
-  theForge
-    .use(watch({
-      paths: {
-        [config.dirs.source + '/**/*']: true,
-        [config.dirs.assets + '/**/*']: true,
-        // TODO: auto-rebuild if layouts change
-        // TODO: auto-rebuild if includes change
-        // Enabling this option just causes metalsmith to try to render the
-        // layouts, instead of the pages that use them.
-        // [config.dirs.layouts + '/**/*']: true
-      }
-    }))
-
-  theForge
-    .build(function(err) {
-      if (err) throw err;
-
-      logInfo('Prototyping started');
-
-      // Start browser-sync once the initial build has completed
-      const serveDir = path.join(process.cwd(), config.dirs.build);
-      browserSync({
-        files: [
-          path.join(serveDir, '*'),
-          path.join(serveDir, '**', '*'),
-        ],
-        cwd: serveDir,
-        watchEvents: [
-          'add', 'change', 'unlink', 'addDir', 'unlinkDir'
-        ],
-        watchOptions: {
-          'ignoreInitial': true
-        },
-        server: {
-          baseDir: serveDir,
-        },
-        open: false
-      });
-    });
 }
--- readme.md
+++ readme.md
@@ -76,15 +76,12 @@ module.exports = {
 
 ...
 
-## To-do
+## To-Do
 
-  - Filter out .js files so Webpack processes them, but we don't get the src files copied over directly by metalsmith.
-
+  1. Move CSS processing to Webpack.
 
   1. Create a mechanism in prototype mode to inject file changes if dependencies change. Use this to re-process the source files if the layouts or includes change, or to re-process the CSS if the tailwind file changes.
 
   1. Prevent metalsmith-in-place from looking at every file - it seems to be ignoring it's pattern option.
-
-  1. Can maybe regenerate CSS when tailwind.js changes in a smoother, faster way. Consider having nodemon ignore tailwind.js. Watch that file. When it changes clear it and the postcss config file from the cache. Re-require the postcss config file, re-create the metalsmith plugin, update the plugin in the metalsmith object, and fake a change to all CSS files so the CSS get's regenerated.
 
   1. Document `forge.config.js`.
